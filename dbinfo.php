d71653ece5cb5972218aeb6b36074a56da0cb374
Jon Einkauf <jeinkauf@amazon.com>
2024-11-12 21:52:36 -0800

    Update dbinfo.php

--- dbinfo.php
+++ dbinfo.php
@@ -3,7 +3,7 @@
 // mysql database info.  Database is hosted online.
 $hostname = "mpio49.db.8688605.hostedresource.com";
 $username="mpio49";
-$password="cl.oud5J";
+$password="xxx";
 $database="mpio49";
 
 ?>
